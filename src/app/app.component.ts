45ed80143966d51abf4b771db465d49a2e41768d
Damian-16 <DamianMorganti16@hotmail.com>
2022-06-26 23:29:41 -0400

    agregado primeros stringinterpolations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  private names = 'nombre privado ';
+  name = 'Nicolas';
+  age = 28;
+  //img = 'https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg';
 }
