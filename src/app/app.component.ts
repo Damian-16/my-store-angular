53e69ea0e31646333952789a0ffdf654c5b6832e
Damian-16 <DamianMorganti16@hotmail.com>
2022-06-28 22:00:21 -0400

    agregados varios cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,25 @@ export class AppComponent {
   private names = 'nombre privado ';
   name = 'Nicolas';
   age = 28;
-  img = 'https://source.unsplash.com/random'
+  img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person={
+    name:'Damian',
+    age:29,
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+  increaseAge(){
+    this.person.age += 1;
+  }
+  onScroll(event: Event){
+ const element = event.target as  HTMLElement;
+ console.log(element.scrollTop)
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
