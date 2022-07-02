9150ccf85cf1da3d66d704c16fae965cf02ce705
Damian-16 <DamianMorganti16@hotmail.com>
2022-07-01 21:57:46 -0400

    agregado ngfor para obj y switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -40,5 +41,43 @@ export class AppComponent {
   deleteName(index: number){
     this.namess.splice(index , 1) // el valor a eliminar y cuantos voy a  eliminar a partir de
   }
+
+
+
+
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 
   }
